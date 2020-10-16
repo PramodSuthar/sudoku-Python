14218037922d9737d4216347e5357d86504a5fd9
PramodSuthar <67489670+PramodSuthar@users.noreply.github.com>
2020-10-16 16:43:39 -0400

    Add files via upload

---
+++ buttonClass.py
@@ -0,0 +1,49 @@
+import pygame
+
+class Button:
+    def __init__(self, x, y, width, height, text= None, color=(255,255,255), highlightedColor=(189,189,189), function = None, params= None):
+        self.image = pygame.Surface((width, height))
+        self.pos = (x, y)
+        self.rect = self.image.get_rect()
+        self.rect.topleft = self.pos
+        self.text = text
+        self.color = color
+        self.highlightedColor = highlightedColor
+        self.function = function
+        self.params = params
+        self.highlighted = False
+        self.width = width
+        self.height = height
+
+    def update(self,mouse):
+        if self.rect.collidepoint(mouse):
+            self.highlighted = True
+        else:
+            self.highlighted = False
+
+    def draw(self, window):
+        if self.highlighted:
+            self.image.fill(self.highlightedColor)
+        else:
+            self.image.fill(self.color)
+        
+        if self.text:
+            self.drawText(self.text)
+        
+        window.blit(self.image, self.pos)
+        
+    def click(self):
+        if self.params:
+            self.function(self.params)
+
+        else:
+            self.function()
+
+    def drawText(self, text):
+        font = pygame.font.SysFont("arial", 20, bold=1)
+        text = font.render(text, False, (0,0,0))
+        width, height = text.get_size()
+        x = (self.width-width)//2
+        y = (self.height-height)//2
+
+        self.image.blit(text, (x,y))
